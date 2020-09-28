ae477fba2a8d30ac8613556c856bbbb30df4bef7
DESKTOP-IU11CJP\batuh <batuhanaydonerg1998@gmail.com>
2020-09-28 06:22:05 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/moonturns/flash_chat_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.moonturns.flash_chat_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FlashChat());
+}
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+          textTheme: TextTheme(
+        bodyText2: TextStyle(color: Colors.black54),
+      )),
+    );
+  }
+}
